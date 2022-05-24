bb3a318a5ce1a5d0a3a5164d74f19b2e8179fff3
Nguyen Vo Thuan <vothuan464.646@gmail.com>
2022-05-24 17:27:03 +0700

    integrate new price api and fix ui

--- components/main/home/priceBoard/index.tsx
+++ components/main/home/priceBoard/index.tsx
@@ -1,54 +1,92 @@
-import React, { FC, useEffect, useState } from 'react';
-import { Empty, Tabs } from 'antd';
-import { Button } from '@styles/styled-components/styledButton';
-import TokenList from './TokenList';
-import request from '@services/apiService';
-import * as qs from 'qs';
-import { SectionHeader } from '@styles/styled-components/styledTabs';
+import React, { FC, useEffect, useState } from "react";
+import { Empty, Tabs } from "antd";
+import { Button } from "@styles/styled-components/styledButton";
+import TokenList from "./TokenList";
+import request from "@services/apiService";
+import * as qs from "qs";
+import { SectionHeader } from "@styles/styled-components/styledTabs";
+import axios from "axios";
 
 const { TabPane } = Tabs;
 
 export const PriceBoard: FC = () => {
-    const [tokenList, setTokenList] = useState([]);
-    const [viewMore, setNumberViewMore] = useState(10);
-    const [sort, setSort] = useState(['holder', 'desc']);
+  const [tokenList, setTokenList] = useState([]);
+  const [viewMore, setNumberViewMore] = useState(10);
+  const [sort, setSort] = useState(["price", "desc"]);
 
-    useEffect(() => {
-        (async () => {
-            const query = qs.stringify({
-                // populate: '*',
-                pagination: {
-                    page: 1,
-                    pageSize: viewMore,
-                },
-                sort: [`${sort[0]}:${sort[1]}`]
-            }, {
-                encodeValuesOnly: true,
-            });
-            await request.get(`/tokens?${query}`).then((res) => {
-                setTokenList(res.data.data);
-            })
-        })();
-    }, [viewMore, sort]);
+//   useEffect(() => {
+//     (async () => {
+//       const query = qs.stringify(
+//         {
+//           sort: [`${sort[0]}:${sort[1]}`],
+//           vs_currency: "usd",
+//           price_change_percentage: "1h,24h,7d",
+//           per_page: viewMore,
+//           page: 1,
+//           order: "market_cap_desc",
+//         },
+//         {
+//           encodeValuesOnly: true,
+//         }
+//       );
+//       await axios
+//         .get(`https://dev-api-dappverse.tokenplay.app/coins/markets?${query}`)
+//         .then((res) => {
+//           // console.log(res.data);
+//           setTokenList(res.data);
+//         });
+//     })();
+//   }, [viewMore, sort]);
 
-    const onChangeTab = (key: any) => {
-        //
+  const UPDATE_INTERVAL_MS = 10000;
+  useEffect(() => {
+    const interval = setInterval(async () => {
+      await updateDataPriceBoard();
+    }, UPDATE_INTERVAL_MS);
+
+    const updateDataPriceBoard = async () => {
+      const query = qs.stringify(
+        {
+          sort: [`${sort[0]}:${sort[1]}`],
+          vs_currency: "usd",
+          price_change_percentage: "1h,24h,7d",
+          per_page: viewMore,
+          page: 1,
+          order: "market_cap_desc",
+        },
+        {
+          encodeValuesOnly: true,
+        }
+      );
+      await axios
+        .get(`https://dev-api-dappverse.tokenplay.app/coins/markets?${query}`)
+        .then((res) => {
+          // console.log(res.data);
+          setTokenList(res.data);
+        });
     };
-    const category = [
-        { tab: 'Cryptocurrencies', key: 'Cryptocurrencies' },
-        { tab: 'DeFi', key: 'defi' },
-        { tab: 'NFT', key: 'nft' },
-        { tab: 'Metaverse', key: 'metaverse' },
-        { tab: 'Polkadot', key: 'polkadot' },
-        { tab: 'BNB Chain', key: 'bnb' },
-        { tab: 'Solana', key: 'solana' },
-        { tab: 'Avalanche', key: 'avalanche' },
-    ];
 
-    return (
-        <section className="main-homepage-tokenranking px-lg-3 px-0">
-            <SectionHeader>Price Board</SectionHeader>
-            {/* <Tabs
+    return () => clearInterval(interval);
+  }, [viewMore, sort]);
+
+  const onChangeTab = (key: any) => {
+    //
+  };
+  const category = [
+    { tab: "Cryptocurrencies", key: "Cryptocurrencies" },
+    { tab: "DeFi", key: "defi" },
+    { tab: "NFT", key: "nft" },
+    { tab: "Metaverse", key: "metaverse" },
+    { tab: "Polkadot", key: "polkadot" },
+    { tab: "BNB Chain", key: "bnb" },
+    { tab: "Solana", key: "solana" },
+    { tab: "Avalanche", key: "avalanche" },
+  ];
+
+  return (
+    <section className="main-homepage-tokenranking px-lg-3 px-0">
+      <SectionHeader>Price Board</SectionHeader>
+      {/* <Tabs
                     defaultActiveKey={"crypto"}
                     onChange={onChangeTab}
                     animated={false}
@@ -61,18 +99,18 @@ export const PriceBoard: FC = () => {
                         )
                     })}
                 </Tabs> */}
-            <TokenList data={tokenList} setSort={setSort} sort={sort} />
-            <br />
-            <div className="text-center">
-                <Button
-                    className="text-green fw-bold fontSize_1-1"
-                    onClick={() => {
-                        setNumberViewMore(viewMore + 10)
-                    }}
-                >
-                    View more
-                </Button>
-            </div>
-        </section>
-    );
+      <TokenList data={tokenList} setSort={setSort} sort={sort} />
+      <br />
+      <div className="text-center">
+        <Button
+          className="text-green fw-bold fontSize_1-1"
+          onClick={() => {
+            if (viewMore < 20) setNumberViewMore(viewMore + 10);
+          }}
+        >
+          View more
+        </Button>
+      </div>
+    </section>
+  );
 };
--- utils/formatCurrency.ts
+++ utils/formatCurrency.ts
@@ -1,9 +1,14 @@
 const currencyFormat = (num: any, fixed: any) => {
     return Number.parseFloat(num).toFixed(fixed).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
 }
+
 export const isExistAndFormatCurrency = (num: any, fixed: any) => {
     num ? num = currencyFormat(num, fixed) : num = 0;
     return num;
 };
+
+export const numberSeperator = (x) => {
+    return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+}
 
 export const formatter = Intl.NumberFormat('en', { notation: 'compact' });
