e0245ee2226f6c5a168ac420d20bfdba7ed12979
Nguyen Vo Thuan <vothuan464.646@gmail.com>
2022-05-26 17:04:22 +0700

    fix bug not enough slides

--- components/main/home/news/index.tsx
+++ components/main/home/news/index.tsx
@@ -1,79 +1,82 @@
-import React, { FC, useEffect, useState } from 'react';
-import { Empty, Tabs } from 'antd';
-import NewsSlides from '../slides/NewsSlides';
-import request from '@services/apiService';
-import * as qs from 'qs';
+import React, { FC, useEffect, useState } from "react";
+import { Empty, Tabs } from "antd";
+import NewsSlides from "../slides/NewsSlides";
+import request from "@services/apiService";
+import * as qs from "qs";
 
 const { TabPane } = Tabs;
 
 export const News: FC = () => {
-    const [newsList, setNewsList] = useState([]);
-    const [category, setCategory] = useState({
-        data: [
-            { name: 'Campaign' },
-            { name: 'Airdrops' },
-            { name: 'Scholarship' },
-        ],
-        key: 'Campaign'
+  const [newsList, setNewsList] = useState([]);
+  const [category, setCategory] = useState({
+    data: [{ name: "Campaign" }, { name: "Airdrops" }, { name: "Scholarship" }],
+    key: "Campaign",
+  });
+
+  useEffect(() => {
+    (async () => {
+      // if (category.key === '') {
+      //     await request.get(`/post-categories`).then(async (response) => {
+      //         const res = response.data.data;
+      //         setCategory({
+      //             data: res,
+      //             key: res[0].attributes.name
+      //         });
+      //     })
+      // }
+      const query = qs.stringify(
+        {
+          populate: "*",
+        //   pagination: {
+        //     page: 1,
+        //     pageSize: 6,
+        //   },
+          filters: {
+            isPinned: {
+              $eq: true,
+            },
+            category: {
+              name: category.key,
+            },
+          },
+        },
+        {
+          encodeValuesOnly: true,
+        }
+      );
+      await request.get(`/posts?${query}`).then((res) => {
+        setNewsList([...res.data.data, ...res.data.data, ...res.data.data]);
+      });
+    })();
+  }, [category]);
+  const onChangeTab = (key: any) => {
+    setCategory({
+      ...category,
+      key,
     });
+  };
 
-    useEffect(() => {
-        (async () => {
-            // if (category.key === '') {
-            //     await request.get(`/post-categories`).then(async (response) => {
-            //         const res = response.data.data;
-            //         setCategory({
-            //             data: res,
-            //             key: res[0].attributes.name
-            //         });
-            //     })
-            // }
-            // const query = qs.stringify({
-            //     populate: '*',
-            //     // pagination: {
-            //     //     page: 1,
-            //     //     pageSize: 6,
-            //     // },
-            //     filters: {
-            //         isPinned: {
-            //             $eq: true
-            //         },
-            //         category: {
-            //             name: category.key
-            //         }
-            //     },
-            // }, {
-            //     encodeValuesOnly: true,
-            // });
-            await request.get(`/posts?populate=%2A&filters[isPinned][$eq]=true&filters[category][name][$eq]=Campaign&sort[0]=createdAt%3Adesc`).then((res) => {
-                setNewsList(res.data.data);
-            });
-        })();
-    }, [category]);
-    const onChangeTab = (key: any) => {
-        setCategory({
-            ...category,
-            key,
-        });
-    };
-
-    return (
-        <section className="main-homepage-news px-lg-3 px-0">
-            <Tabs
-                defaultActiveKey={category.key}
-                onChange={onChangeTab}
-                animated={false}
-                tabBarGutter={1}
-                centered
-            >
-                {category.data && [...category.data].map((cate: any) => {
-                    return (
-                        <TabPane tab={cate.name === 'Campaign' ? 'Campaigns' : cate.name} key={cate.name}>
-                            <NewsSlides data={newsList} tag={category.key} />
-                        </TabPane>
-                    );
-                })}
-            </Tabs>
-        </section>
-    );
+  return (
+    <section className="main-homepage-news px-lg-3 px-0">
+      <Tabs
+        defaultActiveKey={category.key}
+        onChange={onChangeTab}
+        animated={false}
+        tabBarGutter={1}
+        centered
+      >
+        {category.data &&
+          [...category.data].map((cate: any) => {
+            return (
+              <TabPane
+                tab={cate.name === "Campaign" ? "Campaigns" : cate.name}
+                key={cate.name}
+              >
+                <NewsSlides data={newsList} tag={category.key} />
+              </TabPane>
+            );
+          })}
+      </Tabs>
+    </section>
+  );
 };
