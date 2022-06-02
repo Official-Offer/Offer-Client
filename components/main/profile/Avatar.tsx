1fd2e4c534fcb0628aaf11eb423ccd5b1dcb75ef
Nguyen Vo Thuan <vothuan464.646@gmail.com>
2022-06-02 16:50:27 +0700

    add functionality to avatar box

--- components/main/profile/Avatar.tsx
+++ components/main/profile/Avatar.tsx
@@ -8,7 +8,7 @@ import {
 } from "@styles/styled-components/styledUser";
 import { Divider } from "antd";
 import { Heart, Settings } from "react-feather";
-const Avatar = ({data, showFavDapp, setShowFavDapp}) => {
+const Avatar = ({ data, showFavDapp, setShowFavDapp }) => {
   return (
     <AvatarWrapper>
       <AvatarContainer className="row">
@@ -16,13 +16,28 @@ const Avatar = ({data, showFavDapp, setShowFavDapp}) => {
       </AvatarContainer>
       <AvatarName>{data.displayName}</AvatarName>
       <Divider style={{ background: "rgba(29, 187, 189, 0.5)", margin: 0 }} />
-      <div className="avatar-button">
-        {" "}
-        <Heart color={!showFavDapp ? "#1DBBBD" : 'white'} size={20} strokeWidth="2.7px" style={{marginRight:20}}/>
+      <div
+        className={`avatar-button${showFavDapp ? "-active" : ""}`}
+        onClick={() => setShowFavDapp(!showFavDapp)}
+      >
+        <Heart
+          color={!showFavDapp ? "#1DBBBD" : "white"}
+          size={20}
+          strokeWidth="2.7px"
+          style={{ marginRight: 20 }}
+        />
         Favorite Dapps
       </div>
-      <div className="avatar-button-active">
-        <Settings color={!showFavDapp ? "white" : '#1DBBBD'} size={20} strokeWidth="2.7px" style={{marginRight:20}}/>
+      <div
+        className={`avatar-button${!showFavDapp ? "-active" : ""}`}
+        onClick={() => setShowFavDapp(!showFavDapp)}
+      >
+        <Settings
+          color={!showFavDapp ? "white" : "#1DBBBD"}
+          size={20}
+          strokeWidth="2.7px"
+          style={{ marginRight: 20 }}
+        />
         Profile Settings
       </div>
     </AvatarWrapper>
