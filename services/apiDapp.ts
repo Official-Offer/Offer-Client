a2cead05001953e1734decbd523e0a9f5dbb75cd
Nguyen Vo Thuan <vothuan464.646@gmail.com>
2022-06-20 09:28:51 +0700

    remove accesstoken

--- services/apiDapp.ts
+++ services/apiDapp.ts
@@ -2,8 +2,8 @@ import { URL_API_DAPPVERSE } from '@config/index';
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-// const token = Cookies.get("accessToken");
-const token='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiZGlzcGxheU5hbWUiOiJOZ3V5ZW4gSG9uZyBBbmgiLCJ1c2VyQXBpSWQiOjQsImlhdCI6MTY1NTQ1MjE5NiwiZXhwIjoxNjU1NTM4NTk2fQ.d9gvo3021dhMRwEDxvPauA8USYeyNX5w6Zi74ZszPTc'
+const token = Cookies.get("accessToken");
+// const token='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiZGlzcGxheU5hbWUiOiJOZ3V5ZW4gSG9uZyBBbmgiLCJ1c2VyQXBpSWQiOjQsImlhdCI6MTY1NTQ1MjE5NiwiZXhwIjoxNjU1NTM4NTk2fQ.d9gvo3021dhMRwEDxvPauA8USYeyNX5w6Zi74ZszPTc'
 
 export default axios.create({
     baseURL: URL_API_DAPPVERSE,
