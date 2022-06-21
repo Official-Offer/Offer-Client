3b080bd74ea1470ac5efc781e97a7f05543a4a44
Nguyen Vo Thuan <vothuan464.646@gmail.com>
2022-06-21 14:06:47 +0700

    change accesstoken

--- services/apiSSO.ts
+++ services/apiSSO.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 // const token = Cookies.get("accessToken");
-const token='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZGlzcGxheU5hbWUiOiJTdGV2ZSBOZ3V5ZW4iLCJ1c2VyQXBpSWQiOjE2LCJpYXQiOjE2NTU3MDg3NDAsImV4cCI6MTY1NTc5NTE0MH0.vr6mNJKmVBFLrcurG9kOQjz0qmZlv4NSKMEvzEKN-OA'
+const token='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZGlzcGxheU5hbWUiOiJTdGV2ZSBOZ3V5ZW4iLCJ1c2VyQXBpSWQiOjE2LCJpYXQiOjE2NTU3OTUxMDgsImV4cCI6MTY1NTg4MTUwOH0.koD1WHaFAwIw6fk3raTB6vITxN1pPHepj8iP3nU0FZw'
 export default axios.create({
     baseURL: URL_API_SSO,
     headers: {
