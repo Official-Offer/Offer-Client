903048059375958ea20fa1fc2c8f90d26d82ee6b
Nguyen Vo Thuan <vothuan464.646@gmail.com>
2022-06-29 17:18:14 +0700

    rm accesstoken

--- services/apiDapp.ts
+++ services/apiDapp.ts
@@ -2,8 +2,8 @@ import { URL_API_DAPPVERSE } from '@config/index';
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-// const token = Cookies.get("accessToken");
-const token='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZGlzcGxheU5hbWUiOiJTdGV2ZSBOZ3V5ZW4iLCJ1c2VyQXBpSWQiOjE2LCJpYXQiOjE2NTY0ODk2NDUsImV4cCI6MTY1NjU3NjA0NX0.wY_IUNKtshzmyV9YNAyTmAxVbNufx-T_xg6wibTku6s'
+const token = Cookies.get("accessToken");
+// const token='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZGlzcGxheU5hbWUiOiJTdGV2ZSBOZ3V5ZW4iLCJ1c2VyQXBpSWQiOjE2LCJpYXQiOjE2NTY0ODk2NDUsImV4cCI6MTY1NjU3NjA0NX0.wY_IUNKtshzmyV9YNAyTmAxVbNufx-T_xg6wibTku6s'
 
 export default axios.create({
     baseURL: URL_API_DAPPVERSE,
--- services/apiSSO.ts
+++ services/apiSSO.ts
@@ -2,9 +2,9 @@ import { TOKEN_BEARER, URL_API_SSO } from "@config/index";
 import axios from "axios";
 import Cookies from "js-cookie";
 
-// const token = Cookies.get("accessToken");
-const token = 
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZGlzcGxheU5hbWUiOiJTdGV2ZSBOZ3V5ZW4iLCJ1c2VyQXBpSWQiOjE2LCJpYXQiOjE2NTY0ODk2NDUsImV4cCI6MTY1NjU3NjA0NX0.wY_IUNKtshzmyV9YNAyTmAxVbNufx-T_xg6wibTku6s";
+const token = Cookies.get("accessToken");
+// const token = 
+  // "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZGlzcGxheU5hbWUiOiJTdGV2ZSBOZ3V5ZW4iLCJ1c2VyQXBpSWQiOjE2LCJpYXQiOjE2NTY0ODk2NDUsImV4cCI6MTY1NjU3NjA0NX0.wY_IUNKtshzmyV9YNAyTmAxVbNufx-T_xg6wibTku6s";
 export default axios.create({
   baseURL: URL_API_SSO,
   headers: {
