13ea1678a1aa4c17e697b3bcf322c61295f85df1
Nguyen Vo Thuan <vothuan464.646@gmail.com>
2022-07-02 20:25:22 +0700

    rm accesstoken

--- services/apiDapp.ts
+++ services/apiDapp.ts
@@ -2,9 +2,9 @@ import { URL_API_DAPPVERSE } from "@config/index";
 import axios from "axios";
 import Cookies from "js-cookie";
 
-// const token = Cookies.get("accessToken");
-const token =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiZGlzcGxheU5hbWUiOiJOZ3V5ZW4gSG9uZyBBbmgiLCJ1c2VyQXBpSWQiOjQsImlhdCI6MTY1Njc2NzY4NywiZXhwIjoxNjU2ODU0MDg3fQ.1mzHoEMzWUVqpeMJDbjB7eeN82XNqMyJUAEc4_4gA2w";
+const token = Cookies.get("accessToken");
+// const token =
+  // "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiZGlzcGxheU5hbWUiOiJOZ3V5ZW4gSG9uZyBBbmgiLCJ1c2VyQXBpSWQiOjQsImlhdCI6MTY1Njc2NzY4NywiZXhwIjoxNjU2ODU0MDg3fQ.1mzHoEMzWUVqpeMJDbjB7eeN82XNqMyJUAEc4_4gA2w";
 
 export default axios.create({
   baseURL: URL_API_DAPPVERSE,
