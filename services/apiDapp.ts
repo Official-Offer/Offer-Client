37aa8d8ce3fc0fdfbfade6b2d3d5e87240db6653
Nguyen Vo Thuan <vothuan464.646@gmail.com>
2022-07-02 20:20:22 +0700

    fix shit

--- services/apiDapp.ts
+++ services/apiDapp.ts
@@ -4,7 +4,7 @@ import Cookies from "js-cookie";
 
 // const token = Cookies.get("accessToken");
 const token =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiZGlzcGxheU5hbWUiOiJOZ3V5ZW4gSG9uZyBBbmgiLCJ1c2VyQXBpSWQiOjQsImlhdCI6MTY1NjY1MTE3MSwiZXhwIjoxNjU2NzM3NTcxfQ.6gXUQx_zqRcbppG8FF2_EQVP47FC7dI9SE85KO_ELSY";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiZGlzcGxheU5hbWUiOiJOZ3V5ZW4gSG9uZyBBbmgiLCJ1c2VyQXBpSWQiOjQsImlhdCI6MTY1Njc2NzY4NywiZXhwIjoxNjU2ODU0MDg3fQ.1mzHoEMzWUVqpeMJDbjB7eeN82XNqMyJUAEc4_4gA2w";
 
 export default axios.create({
   baseURL: URL_API_DAPPVERSE,
