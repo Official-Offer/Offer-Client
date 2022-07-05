3367103e80781378b52d9d17ab5d73280f1f2e99
Nguyen Vo Thuan <vothuan464.646@gmail.com>
2022-07-05 16:59:45 +0700

    fix shit

--- services/apiSSO.ts
+++ services/apiSSO.ts
@@ -4,7 +4,7 @@ import Cookies from "js-cookie";
 
 // const token = Cookies.get("accessToken");
 const token =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiZGlzcGxheU5hbWUiOiJOZ3V5ZW4gSG9uZyBBbmgiLCJ1c2VyQXBpSWQiOjQsImlhdCI6MTY1NjkyNjMwNSwiZXhwIjoxNjU3MDEyNzA1fQ.sfo-RX4ykb-nmB6CJdWJUGfNy49RWMVtnNxnoYeEex8"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiZGlzcGxheU5hbWUiOiJOZ3V5ZW4gSG9uZyBBbmgiLCJ1c2VyQXBpSWQiOjQsImlhdCI6MTY1NzAxNDUxNSwiZXhwIjoxNjU3MTAwOTE1fQ.chv9vu-fG_pt-0FQ4KuIAYuNK1sO5s_Xvf4Tt8h3qEU"
 export default axios.create({
   baseURL: URL_API_SSO,
   headers: {
