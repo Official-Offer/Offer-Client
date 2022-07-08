1e4e7791b8d7f700dbf622d05ea597a4562af885
Nguyen Vo Thuan <vothuan464.646@gmail.com>
2022-07-08 14:10:50 +0700

    rm ac

--- services/apiSSO.ts
+++ services/apiSSO.ts
@@ -2,9 +2,9 @@ import { TOKEN_BEARER, URL_API_SSO } from "@config/index";
 import axios from "axios";
 import Cookies from "js-cookie";
 
-// const token = Cookies.get("accessToken");
-const token =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiZGlzcGxheU5hbWUiOiJOZ3V5ZW4gSG9uZyBBbmgiLCJ1c2VyQXBpSWQiOjQsImlhdCI6MTY1NzI1Njk2MywiZXhwIjoxNjU3MzQzMzYzfQ.xvohdcUpeiPGfAxuW5rAw-2O6KZ6eOLoL2LuZD3xgvE"
+const token = Cookies.get("accessToken");
+// const token =
+  // "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiZGlzcGxheU5hbWUiOiJOZ3V5ZW4gSG9uZyBBbmgiLCJ1c2VyQXBpSWQiOjQsImlhdCI6MTY1NzI1Njk2MywiZXhwIjoxNjU3MzQzMzYzfQ.xvohdcUpeiPGfAxuW5rAw-2O6KZ6eOLoL2LuZD3xgvE"
 export default axios.create({
   baseURL: URL_API_SSO,
   headers: {
