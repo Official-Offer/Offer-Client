94d53de37fc2d3f63cad90e0d21c4ebc940419e0
kien-to <ktto@umass.edu>
2023-02-23 12:12:10 -0500

    hehe

--- src/common/LayoutGlobal.tsx
+++ src/common/LayoutGlobal.tsx
@@ -11,6 +11,7 @@ export default function LayoutGlobal(props: any): ReactElement {
     <>
       <Navbar />
       {/* Stupid shit */}
+      {/* Another stupid shit */}
       <div>{props.children}</div>
     </>
   );
