260cae8d92e9211dbb751860de332aaae2e8ab15
kien-to <ktto@umass.edu>
2023-02-24 14:12:28 -0500

    Add left panel styled component

--- pages/student/email/index.tsx
+++ pages/student/email/index.tsx
@@ -1,11 +1,23 @@
 import EmailForm from "@components/forms/EmailForm";
 import { NextPage } from "next";
+import { LeftPanel } from "@styles/styled-components/styledDiv";
 
 //create a next page for the student home page, code below
 const StudentEmail: NextPage = () => {
   return (
-    <div>
-      <EmailForm onSubmit={(email)=>{return}}/>
+    <div className="student-email">
+      <div className="student-email-sideBar">
+       <LeftPanel> </LeftPanel>
+      </div>
+      <div className="student-email-content">
+        <div className="student-email-content-form">
+          <EmailForm
+            onSubmit={(email) => {
+              return;
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/forms/EmailForm.tsx
+++ src/components/forms/EmailForm.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { FormInput } from "@styles/styled-components/styledForm";
+import { SubmitButton } from "@styles/styled-components/styledButton";
+import { Typography } from "antd";
 
 interface IEmailForm {
   onSubmit: (email: string) => void;
@@ -20,19 +22,26 @@ function EmailForm({ onSubmit }: IEmailForm) {
   return (
     <form onSubmit={handleSubmit}>
       <label>
-        Nhập email của bạn:
+        <b> Nhập email của bạn: </b>
       </label>
+      <br />
       <FormInput
-          width="250px"
-          type="email"
-          value={email}
-          onChange={handleEmailChange}
-          required
-        />
-      <button type="submit">Submit</button>
+        width="250px"
+        type="email"
+        value={email}
+        onChange={handleEmailChange}
+        required
+      />
+      <br />
+      <Typography.Text type="secondary">
+        Sử dụng mail .edu sẽ giúp quá trình <br />
+        xác thực được nhanh chóng hơn.
+      </Typography.Text>
+      <br />
+      <br />
+      <SubmitButton type="submit">Tiếp tục</SubmitButton>
     </form>
   );
 }
 
 export default EmailForm;
-
