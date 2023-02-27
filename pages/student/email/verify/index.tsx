12fb9d6f32cbf5629fff8d3edb0d098ec0babb8d
kien-to <ktto@umass.edu>
2023-02-26 22:00:51 -0500

    verification screen

--- pages/student/email/verify/index.tsx
+++ pages/student/email/verify/index.tsx
@@ -1,9 +1,10 @@
 import { NextPage } from "next";
 import { LeftPanel } from "@styles/styled-components/styledDiv";
-import SchoolForm from "@components/forms/SchoolForm";
 import { useRouter } from "next/router";
-
-//create a next page for the student home page, code below
+import PinInput from "react-pin-input";
+import { Typography } from "antd";
+import { SubmitButton } from "@styles/styled-components/styledButton";
+import Link from "next/link";
 const EmailVerify: NextPage = () => {
   const router = useRouter();
 
@@ -14,7 +15,52 @@ const EmailVerify: NextPage = () => {
       </div>
       <div className="register-student-content">
         <div className="register-student-content-form">
-          <h1>Kien To</h1>
+          <h1>Bách Khoa Hà Nội</h1>
+          <p>
+            Mã xác nhận đã được gửi tới email <b>kiento0905.hec@gmail.com</b>
+          </p>
+          <div className="register-student-content-form-pincode">
+            <PinInput
+              length={4}
+              initialValue=""
+              onChange={(value, index) => {}}
+              type="numeric"
+              inputMode="number"
+              style={{ padding: "20px 0px" }}
+              inputStyle={{
+                borderColor: "#7277F1",
+                fontSize: "20px",
+                width: "50px",
+              }}
+              inputFocusStyle={{ borderColor: "#D30B81" }}
+              onComplete={(value, index) => {}}
+              autoSelect={true}
+              regexCriteria={/^[ A-Za-z0-9_@./#&+-]*$/}
+            />
+            <Typography.Text type="secondary">
+              Không nhận được? <u>Gửi lại mã xác nhận</u>
+            </Typography.Text>
+            <br />
+            <br />
+            <SubmitButton type="submit">Tiếp tục</SubmitButton>
+            <br />
+            <br />
+            <hr />
+            <Typography.Text type="secondary">
+              Đã có tài khoản? <br />
+              <Typography.Text underline>
+                <Link href="/student/login">Đăng nhập tại đây</Link>
+              </Typography.Text>
+            </Typography.Text>
+            <br />
+            <br />
+            <Typography.Text type="secondary">
+              Bạn là nhà tuyển dụng? <br />
+              <Typography.Text underline>
+                <Link href="/student/login">Đăng ký/Đăng nhập tại đây</Link>
+              </Typography.Text>
+            </Typography.Text>
+          </div>
         </div>
       </div>
     </div>
