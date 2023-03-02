5a93ea1d193b72c8d04b1fa10f47e753b91e60e3
Nguyen Vo Thuan <vothuan464.646@gmail.com>
2023-03-02 18:04:39 -0500

    shit

--- pages/student/jobs/index.tsx
+++ pages/student/jobs/index.tsx
@@ -1,10 +1,14 @@
 import { NextPage } from "next";
 import { JobFilterNavbar } from "@components/navbar/JobFilterNavBar";
+import { Row } from "antd";
 //create a next page for the student home page, code below
 const StudentJobs: NextPage = () => {
   return (
     <div>
       <JobFilterNavbar />
+      <Row>
+        
+      </Row>
       <h1>StudentJobs</h1>
     </div>
   );
