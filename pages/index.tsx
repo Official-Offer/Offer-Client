d800f776b705a485ead98d4f11305a03edefa275
Bao Dang <85828374+weebao@users.noreply.github.com>
2023-03-02 09:26:34 -0500

    Comment out direct import and add dynamic import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,26 @@
 import React from "react";
 import type { NextPage } from 'next';
 import dynamic from 'next/dynamic';
-import { Header, Main, CardsGrid, Footer } from "@components";
+// import { Header, Main, CardsGrid, Footer } from "@components";
 import Counter from "@components/examples/counter";
 import { StyledSystemExample } from "@components/examples/styled-system";
 import { TestingLibraryExample } from "@components/examples/testing-library";
 import { I18NExample } from "@components/examples/translate";
 const Home: NextPage = () => {
-  // const Navbar = dynamic(() => import("@components").then((mod: any) => mod.Navbar)) as any;
-  // const Header = dynamic(() => import("@components").then((mod: any) => mod.Header)) as any;
+  const Navbar = dynamic(() => import("@components").then((mod: any) => mod.Navbar)) as any;
+  const Header = dynamic(() => import("@components").then((mod: any) => mod.Header)) as any;
   const Main = dynamic(() => import("@components").then((mod: any) => mod.Main)) as any;
-  // const CardsGrid = dynamic(() => import("@components").then((mod: any) => mod.CardsGrid)) as any;
-  // const Footer = dynamic(() => import("@components").then((mod: any) => mod.Footer)) as any;
+  const CardsGrid = dynamic(() => import("@components").then((mod: any) => mod.CardsGrid)) as any;
+  const Footer = dynamic(() => import("@components").then((mod: any) => mod.Footer)) as any;
   return (
     <div
       style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
     >
-      {/* <Header /> */}
-      {/* <Navbar /> */}
+      <Header />
+      <Navbar />
       <Main /> 
-      {/* <CardsGrid /> */}
-      {/* <Footer /> */}
+      <CardsGrid />
+      <Footer />
       {/* <Counter /> */}
     </div>
     // <Counter />
