be5463253768c5294516d386f1d905fca9158c67
Nguyen Vo Thuan <vothuan464.646@gmail.com>
2023-03-03 17:37:13 -0500

    scrollbar finish

--- pages/student/jobs/index.tsx
+++ pages/student/jobs/index.tsx
@@ -3,18 +3,21 @@ import { JobFilterNavbar } from "@components/navbar/JobFilterNavBar";
 import { JobCard } from "@components/cardsGrid/JobCard";
 import { Row, Col } from "antd";
 import { JobContent } from "@components/main/JobContent";
+import { useState } from "react";
 //create a next page for the student home page, code below
 const StudentJobs: NextPage = () => {
   const jobsList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  const [activeCardIndex, setActiveCardIndex] = useState(0);
   return (
     <div>
       <JobFilterNavbar />
+      <div className="job-portal-fusion"></div>
       <Row>
         <Col span={6}>
           <div className="job-portal-list">
             <div className="job-portal-list-result">20k kết quả</div>
-            {jobsList.map((job) => (
-              <JobCard />
+            {jobsList.map((job, i) => (
+              <JobCard active={i===activeCardIndex} onClick={()=>setActiveCardIndex(i)}/>
             ))}
           </div>
         </Col>
--- src/components/cardsGrid/JobCard.tsx
+++ src/components/cardsGrid/JobCard.tsx
@@ -1,14 +1,27 @@
 import { Col, Row, Button } from "antd";
 import { TagOutlined } from "@ant-design/icons";
-export const JobCard: React.FC = () => {
+import { ReactNode } from "react";
+type JobCardProp = {
+  children?: ReactNode;
+  active: boolean; //use this to pass in parameters from the job site
+  onClick: () => void;
+};
+export const JobCard: React.FC<JobCardProp> = ({ active, onClick }) => {
+  const cardClassName = active
+    ? "job-portal-list-card active"
+    : "job-portal-list-card";
   return (
-    <div className="job-portal-list-card">
+    <div className={cardClassName}>
       <div className="job-portal-list-card-title">
         <Row>
-          <Col span={20}>Thực tập sinh kỹ sư phần mềm chi nhánh TP.HCM</Col>
-          <Col span={3}>
-            <Button icon={<TagOutlined size={100}/>} />
+          <Col span={20} onClick={onClick}>
+            Thực tập sinh kỹ sư phần mềm chi nhánh TP.HCM
           </Col>
+          {!active && (
+            <Col span={3}>
+              <Button icon={<TagOutlined style={{ fontSize: 17 }} />} />
+            </Col>
+          )}
         </Row>
       </div>
       <div className="job-portal-list-card-detail">Samsung</div>
