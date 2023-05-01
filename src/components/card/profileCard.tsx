186cb3be46e47a3d6fb4886df33f43081d117132
Bao Dang <baochidangg@gmail.com>
2023-05-01 00:15:17 -0400

    Add API functions + Customize reusable ProfileCard for both

--- src/components/card/profileCard.tsx
+++ src/components/card/profileCard.tsx
@@ -1,28 +1,31 @@
+import  React, { useState } from "react";
+import { useQuery, useMutation } from "react-query";
 import { Card as AntdCard, Button } from "antd";
 import { ArrowLeftOutlined, ArrowRightOutlined, PlusOutlined, EditOutlined } from "@ant-design/icons";
 
 type ProfileCardProps = {
   fieldTitle: string,
-  itemList?: [
-    {
-      name: string,
-      logoURL?: string,
-      dateLabel: string,
-      dates?: [number, number],
-      infoList?: [
-        {
-          label: string;
-          details: string;
-        }
-      ]
-    }
-  ]
+  getFunction: () => Record<string, unknown>[],
+  addFunction: (input: Record<string, unknown>) => Record<string, unknown>,
+  editFunction: (input: Record<string, unknown>) => Record<string, unknown>,
+  deleteFunction: (input: Record<string, unknown>) => Record<string, unknown>,
 }
 
-export const ProfileCard: React.FC<ProfileCardProps> = ({ fieldTitle, itemList }) => {
+export const ProfileCard: React.FC<ProfileCardProps> = ({ fieldTitle, getFunction, addFunction, editFunction, deleteFunction }) => {
+  const logoURL = "https://upload.wikimedia.org/wikipedia/vi/thumb/e/ef/Logo_%C4%90%E1%BA%A1i_h%E1%BB%8Dc_B%C3%A1ch_Khoa_H%C3%A0_N%E1%BB%99i.svg/1200px-Logo_%C4%90%E1%BA%A1i_h%E1%BB%8Dc_B%C3%A1ch_Khoa_H%C3%A0_N%E1%BB%99i.svg.png";
+
+  const [itemList, setItemList] = useState<Record<string, unknown>[]>([]);
+  const getItems = useQuery({
+    queryKey: fieldTitle,
+    queryFn: getFunction,
+    onSuccess: (res) => setItemList(res),
+    onError: (err) => console.log(`Error: ${err}`),
+  });
+
   return (
     <AntdCard
       className="main-panel-card"
+      loading={getItems.isLoading}
       title={
         <div className="main-panel-header">
           <h2>{fieldTitle}</h2>
@@ -31,11 +34,11 @@ export const ProfileCard: React.FC<ProfileCardProps> = ({ fieldTitle, itemList }
       }
       children={
         <div className="main-panel-layout">
-          {
+          { itemList.length === 0 ? <div>Xin hãy thêm thông tin vào đây</div> :
             itemList.map((item) => (
               <div className="main-panel-info">
                 <div className="main-panel-info-logo">
-                  {item.logoURL && <img src={item.logoURL} alt={"Logo of " + item.name} />}
+                  <img src={logoURL} alt={"Logo of " + item.name} />
                 </div>
                 <div className="main-panel-info-center">
                   <h3>{item.name}</h3>
