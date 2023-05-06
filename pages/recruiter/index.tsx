96a8115411616e188f922ae07eb843536a6d7c55
kien-to <ktto@umass.edu>
2023-05-06 10:40:18 -0400

    doi lane

--- pages/recruiter/index.tsx
+++ pages/recruiter/index.tsx
@@ -1,15 +1,18 @@
 import { NextPage } from "next";
-import dynamic from "next/dynamic";
+// import dynamic from "next/dynamic";
 
 //create a next page for the student home page, code below
 const Recruiter: NextPage = () => {
-  const ApplicantTable = dynamic(() =>
-    import("@components").then((mod: any) => mod.ApplicantTable)
-  ) as any;
+//   const ApplicantTable = dynamic(() =>
+//     import("@components").then((mod: any) => mod.ApplicantTable)
+//   ) as any;
 
   return (
-    <div>
+    <div className = "">
+      <h1>Dashboard</h1>
+      {/* <div>
       <ApplicantTable />
+      </div> */}
     </div>
   );
 };
