b163741136b03b7ecc1a815e2825bf95c8d0571c
kien-to <ktto@umass.edu>
2023-05-05 20:07:07 -0400

    Add table

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,3 @@
-import Counter from "./examples/counter";
-// import VerticalNav from '@components/navbar/VerticalNav';
 export { Navbar } from "./navbar";
 export { VerticalNav } from "./navbar/VerticalNav";
 export { Logo } from "./logo";
@@ -10,5 +8,5 @@ export { CardsGrid } from "./cardsGrid";
 export { CardTray } from "./cardTray";
 export { ResumePanel } from "./resumePanel";
 export { Footer } from "./footer";
-
+export { ApplicantTable } from "./table/applicantTable";
 // asdda
