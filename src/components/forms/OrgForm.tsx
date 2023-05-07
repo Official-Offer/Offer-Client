80ccce7b81c15ed5fbbca919905314deb7989d7b
kien-to <ktto@umass.edu>
2023-05-07 14:12:57 -0400

    fix orgForm

--- src/components/forms/OrgForm.tsx
+++ src/components/forms/OrgForm.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { FormInput } from "@styles/styled-components/styledForm";
 // import { SubmitButton } from "@styles/styled-components/styledButton";
-import { Typography } from "antd";
+import { Form, Select, Typography } from "antd";
 import { FootnoteForm } from "./FootnoteForm";
 import Link from "next/link";
 import { useSelector } from "react-redux";
@@ -9,67 +9,78 @@ import { RootState } from "@redux/reducers";
 import { SubmitButton } from "@components/button/SubmitButton";
 
 interface IOrgForm {
-  onSubmit: (org: string) => void,
+  onSubmit: (org: string) => void;
   isLoading: boolean;
 }
 
-export const OrgForm: React.FC<IOrgForm> = ({ onSubmit, isLoading }: IOrgForm) => {
+export const OrgForm: React.FC<IOrgForm> = ({
+  onSubmit,
+  isLoading,
+}: IOrgForm) => {
   const [Org, setOrg] = useState("");
   const state = useSelector((state: RootState) => state.account);
-
+  const [schools, setSchools] = useState([
+    "Bách Khoa",
+    "Sư Phạm",
+    "Ngoại Thương",
+  ]);
+  const [companies, setCompanies] = useState(["OpenAI", "Google", "Facebook"]);
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     onSubmit(Org);
   };
 
-  const handleOrgChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setOrg(event.target.value);
+  const handleOrgChange = (value: any) => {
+    setOrg(value);
   };
 
   return (
-    <div>
-      <form className="form" onSubmit={handleSubmit}>
-         <div className="form-flex">
-          <div className="form-input">
-            <label>
-              <b>
-                {" "}
-                {state.role.isStudent || state.role.isAdvisor
-                  ? "Kết nối với trường của bạn:"
-                  : "Kết nối với công ty của bạn:"}{" "}
-              </b>
-            </label>
-            <FormInput
-              width="250px"
-              list={state.role.isStudent || state.role.isAdvisor ? "schoolSuggestions" : "companySuggestions"}
-              value={Org}
+    <Form className="form" onSubmit={handleSubmit}>
+      <div className="form-flex">
+        <div className="form-input">
+          <label>
+            <b>
+              {" "}
+              {state.role.isStudent || state.role.isAdvisor
+                ? "Kết nối với trường của bạn:"
+                : "Kết nối với công ty của bạn:"}{" "}
+            </b>
+          </label>
+          <Form.Item>
+            <Select
+              className="form-select"
+              bordered={false}
               onChange={handleOrgChange}
-              required
-            />
-          </div>
-          <datalist id="schoolSuggestions">
-            <option value="Bách Khoa" />
-            <option value="Sư Phạm" />
-            <option value="Ngoại Thương" />
-            <option value="Kinh Tế Quốc Dân" />
-            <option value="FPT" />
-            <option value="VinUniversity" />
-            <option value="RMIT" />
-            <option value="UMass" />
-          </datalist>
-          <datalist id="companySuggestions">
-            <option value="Tesla" />
-            <option value="Vingroup" />
-            <option value="Google Brain" />
-            <option value="OpenAI" />
-          </datalist>
+            >
+              {state.role.isStudent || state.role.isAdvisor
+                ? schools.map((school) => (
+                    <Select.Option
+                      className="form-select-dropdown"
+                      value={school}
+                    >
+                      {school}
+                    </Select.Option>
+                  ))
+                : companies.map((company) => (
+                    <Select.Option
+                      className="form-select-dropdown"
+                      value={company}
+                    >
+                      {company}
+                    </Select.Option>
+                  ))}
+            </Select>
+          </Form.Item>
         </div>
-        {/* <SubmitButton type="submit" className="form-submit-button">
-          Tiếp tục
-        </SubmitButton> */}
-        <br/>
-        <div><SubmitButton text="Tiếp tục" isLoading={isLoading} onClick={handleSubmit}/></div>
-      </form>
-    </div>
+      </div>
+      <br />
+      <div>
+        <SubmitButton
+          text="Tiếp tục"
+          isLoading={isLoading}
+          onClick={handleSubmit}
+        />
+      </div>
+    </Form>
   );
 };
