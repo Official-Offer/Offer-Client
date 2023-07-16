b6ab3d7fac34a79ccab95028e76dfe01999a590f
kien-to <ktto@umass.edu>
2023-07-15 21:14:45 -0700

    add several datatypes

--- src/components/table/dataType.ts
+++ src/components/table/dataType.ts
@@ -3,7 +3,7 @@ export interface JobDataType {
   ID: string;
   posted_date: string;
   title: string;
-  unapproved_schools: number;
+  unapproved_schools: number; 
   approved_schools: number;
   no_applicants: number;
   expected: number;
@@ -35,29 +35,95 @@ export interface ApprovedJobDataType {
 }
 
 export interface ApplicantDataType {
+  key: string | null;
   ID: string | null;
   name: string | null;
   school: string | null;
   major: string | null;
   expected_graduation: string | null;
+  compatibility: string; //percentage
   tag: string | null;
 }
 
 export interface StudentDataType {
+  key: string | null;
   ID: string | null;
   name: string | null;
-  school: string | null;
   major: string | null;
   expected_graduation: string | null;
-  tag: string | null;
+  jobs_applied: number | null;
+  jobs_accepted: number | null;
+  // tag: string | null;
+}
+
+
+export interface AdvisorCompanyDataType {
+  key: string | null;
+  ID: string | null;
+  name: string | null;
+  major: string | null;
+  expected_graduation: string | null;
+  jobs_applied: number | null;
+  jobs_accepted: number | null;
+  // tag: string | null;
+}
+
+export interface AdvisorSchoolDataType {
+  key: string | null;
+  ID: string | null;
+  name: string | null;
+  major: string | null;
+  expected_graduation: string | null;
+  jobs_applied: number | null;
+  jobs_accepted: number | null;
+  // tag: string | null;
+}
+
+export interface RecruiterSchoolDataType {
+  key: string | null;
+  ID: string | null;
+  name: string | null;
+  major: string | null;
+  expected_graduation: string | null;
+  jobs_applied: number | null;
+  jobs_accepted: number | null;
+  // tag: string | null;
+}
+
+export interface RecruiterCompanyDataType {
+  key: string | null;
+  ID: string | null;
+  name: string | null;
+  major: string | null;
+  expected_graduation: string | null;
+  jobs_applied: number | null;
+  jobs_accepted: number | null;
+  // tag: string | null;
 }
 
 export interface SchoolDataType {
+  key: string | null;
   ID: string | null;
   name: string | null;
-  noStudents: number | 0;
-  noJobs: number | 0;
-  tag: string | null;
+  advisors: string | null;
+  no_students: number | 0;
+  no_employees: number | 0;
+  unapproved_jobs: number | 0;
+  approved_jobs: number | 0;
+  compatibility: string | null;
+  // tag: string | null;
+}
+
+export interface CompanyDataType {
+  key: string | null;
+  ID: string | null;
+  name: string | null;
+  //Should be changed to images + numbers
+  recruiters: string | null;
+  unverified_jobs: number | 0;
+  verifiedJobs: number | 0;
+  students: number | 0;
+  events: number | 0;
 }
 
 export interface EventRecruiterDataType {
@@ -76,13 +142,3 @@ export interface EventAdvisorDataType {
   tag: string | null;
 }
 
-export interface CompanyDataType {
-  ID: string | null;
-  name: string | null;
-  //Should be changed to images + numbers
-  recruiters: number | 0;
-  unverifiedJobs: number | 0;
-  verifiedJobs: number | 0;
-  students: number | 0;
-  events: number | 0;
-}
