dbc08b23eb201fc342c4103bcedeed6fffd5bd56
Nguyen Vo Thuan <vothuan464.646@gmail.com>
2024-01-15 09:42:45 +0700

    fix schoolist

--- public/static/list/schoolList.ts
+++ public/static/list/schoolList.ts
@@ -1,4 +1,5 @@
 export const schoolList = [
+  { id: 5, name: "Đại học Kinh Tế Tài Chính"},
   { id: 4, name: "Fulbright University Vietnam" },
   { id: 3, name: "UMass Amherst" },
   { id: 2, name: "Harvard" },
